c9d655c9d7e74950eb13f807fa315a820d37deb0
mjmgzz <111893166+mjmgzz@users.noreply.github.com>
2022-12-15 20:06:52 -0600

    fixed oopsie

--- gallery.js
+++ gallery.js
@@ -1,34 +1,43 @@
 "use strict";
 
-var pics = document.getElementsByClassName("gallery-image");
+var photoDivs = document.getElementsByClassName("gallery-image");
+
 var nextButton = document.getElementById("button_next");
-var previousButton = document.getElementById("button-previous");
+
+var previousButton = document.getElementById("button_previous");
+
 var photoNumber = document.getElementById("stepper");
+
 var currentPhotoNumber = 0;
 
-pics[currentPhotoNumber].classList.remove('hideThis');
-photoNumber.innerHTML = (currentPhotoNumber + 1) + " out of " + pics.length;
+photoDivs[currentPhotoNumber].classList.remove('hideThis');
 
-nextButton.addEventListener('click,' function() {
-  pics[currentPhotoNumber].classList.add("hideThis");
+photoNumber.innerHTML = (currentPhotoNumber + 1) + " out of " + photoDivs.length;
+
+nextButton.addEventListener('click', function() {
+  photoDivs[currentPhotoNumber].classList.add("hideThis");
   currentPhotoNumber = currentPhotoNumber + 1;
-  if (currentPhotoNumber === pics.length) {
-    currentPhotoNumber=0;
+
+  if (currentPhotoNumber === photoDivs.length) {
+    currentPhotoNumber = 0;
   }
 
-  pics[currentPhotoNumber].classList.remove('hideThis');
-  photoNumber.innerHTML = (currentPhotoNumber + 1) + " out of " + pics.length;
+  photoDivs[currentPhotoNumber].classList.remove('hideThis');
+
+  photoNumber.innerHTML = (currentPhotoNumber + 1) + " out of " + photoDivs.length;
 
 });
 
-previousButton.addEventListener('click,' function() {
-  pics[currentPhotoNumber].classList.add("hideThis");
+previousButton.addEventListener('click', function() {
+  photoDivs[currentPhotoNumber].classList.add("hideThis");
   currentPhotoNumber = currentPhotoNumber - 1;
+
   if (currentPhotoNumber < 0) {
-    currentPhotoNumber= pics.length - 1;
+    currentPhotoNumber = photoDivs.length - 1;
   }
 
-  pics[currentPhotoNumber].classList.remove('hideThis');
-  photoNumber.innerHTML = (currentPhotoNumber + 1) + " out of " + pics.length;
+  photoDivs[currentPhotoNumber].classList.remove('hideThis');
+
+  photoNumber.innerHTML = (currentPhotoNumber + 1) + " out of " + photoDivs.length;
 
 });
